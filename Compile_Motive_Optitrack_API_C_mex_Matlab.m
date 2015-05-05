cd81435bad060b9b8f9c8af0e05feea8a6157d8e
or hirshfeld <orhirshfeld@gmail.com>
2015-05-05 11:14:54 +0300

    first upload
    first upload

---
+++ Compile_Motive_Optitrack_API_C_mex_Matlab.m
@@ -0,0 +1,6 @@
+%% compile and run
+%% Compile
+cd('C:\Users\sasson\OneDrive\work control lab aero summer 2014 technion\Motive-Optitrack-API-C-mex-Matlab-')
+mex -v Motive_Optitrack_API_C_mex_Matlab.cpp '-lNPTrackingToolsx64'
+%% Run
+Motive_Optitrack_API_C_mex_Matlab()
